6aa7288b8dcf0cb9e6fc56f5f2fa4a184f618196
Cua Dev <dongdun9a4@gmail.com>
2024-01-09 10:03:07 +0700

    add get address, logo and phone number

--- get_data_yellow_page.py
+++ get_data_yellow_page.py
@@ -49,78 +49,35 @@ class Company(Crawler):
                 website_company = "None"
 
             # Get authentication
-            authen_tag = tag.find("span", {"class": "duocxacthuc"})
-            if authen_tag is not None:
-                get_authentication = authen_tag.getText()
-            else:
-                get_authentication = "Not Authentication"
+            authen_tag = tag.find("p", {"class": "m-0 pb-0 pt-1 hienthi_pc"})
+            get_authentication = authen_tag.getText()
 
             # Get contact
-
             get_contact = tag.find_all("div", {"class": "yp_div_logo_diachi clearfix"})
+
+            # logo and address
             if len(get_contact) != 0:
                 p_tags = get_contact[0].find_all("p", {"class": "m-0"})
+                logo = p_tags[0].find("img").get("src")
+                address = p_tags[1].getText()
+                phone_number = p_tags[2].getText()
 
-                match len(p_tags):
-                    case 6:
-                        logo = p_tags[0].get("src")
-                        address = p_tags[1].getText()
-                        phone_number = p_tags[2].getText() + p_tags[3].getText()
-                        mail = p_tags[4].getText()
-                        print(logo, address, phone_number, mail)
-
-                    case 5, 4:
-                        logo = p_tags[0].get("src")
-                        address = p_tags[1].getText()
-                        phone_number = p_tags[2].getText()
-                        mail = p_tags[3].getText()
-                        print(logo, address, phone_number, mail)
-
-                    case 3:
-                        logo = p_tags[0].get("src")
-                        address = p_tags[1].getText()
-                        phone_number = p_tags[2].getText()
-                        mail = "None"
-                        print(logo, address, phone_number, mail)
-
-                # Get logo
-                # get_logo = get_contact[0].find("img").get("src")
-
-                # # Get contact
-                # container_contact = get_contact[0].find(
-                #     "div", {"class": "float-end yp_diachi_logo"}
-                # )
-                # address = container_contact
-
-            # print(get_logo)
-
-            # if len(get_contact) == 0:
-            #     get_contact = tag.find_all("div", {"class": "h-auto clearfix mt-3"})
-            #     # if len(get_contact) == 2:
-            #     #     print("hi")
-            #     #     address = (
-            #     #         get_contact[0].getText() + get_contact[0].find("span").getText()
-            #     #     )
-            #     #     print(address)
-
-            # Get address
-            # address = get_contact[0].getText() + get_contact[0].find("span").getText()
-
-            # # Get phone number
-            # phone_number = contact[1].getText()
-
-            # # Get mail
-            # mail = contact[2].getText()
-
-            # print(address)
+            # not logo but address
+            else:
+                get_contact = tag.find_all("div", {"class": "h-auto clearfix mt-3"})
+                p_tags = get_contact[0].find_all("p", {"class": "m-0"})
+                logo = "None"
+                address = p_tags[0].getText()
+                phone_number = p_tags[1].getText()
 
             company_list.append(
                 {
-                    "Company": get_name_company,
-                    "Website": website_company,
-                    "Authentication": get_authentication,
-                    # "Logo": get_logo
-                    # "Address": address,
+                    "company": get_name_company,
+                    "website": website_company,
+                    "authentication": get_authentication,
+                    "logo": logo,
+                    "address": address,
+                    "phone_number": phone_number,
                 }
             )
         return company_list
@@ -155,7 +112,7 @@ for page_number in range(1, TOTAL_PAGE_NUMBER + 1):
         for num_page_industry in range(1, total_page_industry + 1):
             company = Company(Industry[0], num_page_industry)
             total_company_number.extend(company.get_company_list())
-            break
+
         break
     break
-# export_excel(total_company_number)
+export_excel(total_company_number)
